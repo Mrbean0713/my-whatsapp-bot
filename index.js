0fe5d4c6941284e7e45f9a1a8335ba7d5170ec9c
Maximilien <Mounkatuil@gmail.com>
2026-06-22 16:09:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Bot is alive");
+}).listen(process.env.PORT || 3000);
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("@whiskeysockets/baileys");
 
 async function startBot() {
