53e3af2649a83e16eeb89c1be7bd12f112755639
Maximilien <Mounkatuil@gmail.com>
2026-06-22 18:00:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ console.log("Bot lancé");
 setInterval(() => {
   console.log("Je tourne toujours");
 }, 60000);
+
+"dependencies": {
+  "@whiskeysockets/baileys": "latest",
+  "express": "latest"
+}
