a68d0b770e03d27283f017c375ae3a73ab6a9451
Maximilien <Mounkatuil@gmail.com>
2026-06-22 18:00:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+app.get("/", (req, res) => {
+    res.send("Bot WhatsApp actif");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Serveur actif sur le port " + PORT);
+});
+const express = require("express");
+const app = express();
 console.log("Bot lancé");
 
 setInterval(() => {
